d66b3d796b0de0e334b64687cba26d4b74defbb9
Lucas Ramos <lucasramos.lr260@gmail.com>
2023-04-14 22:26:42 -0300

    Solução do exercício

---
+++ index.js
@@ -0,0 +1,8 @@
+let Nome = prompt ("Digete seu primeiro nome");
+let Sobrenome = prompt ("Digite seu sobrenome");
+
+let nomeCompleto = `${Nome} ${Sobrenome}`;
+let nomeCatalogo = `${Sobrenome.toUpperCase()} ${Nome}`;
+
+alert (nomeCompleto);
+alert (nomeCatalogo);
